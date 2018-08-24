1ceabbfdac55470b9f9190505cbeb04b0bb8a037
GrassSnakeCode <emilshiq@gmail.com>
2018-08-24 21:55:11 +0200

    program using the enum variable
    Submission and accurate validation must be further developed

--- main.cpp
+++ main.cpp
@@ -1,9 +1,75 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
-int main()
+enum dayofweek{monday=1,tuesday,wednesday=20,thursday,friday,saturday,sunday};
+void menu()
 {
-    cout << "Hello world!" << endl;
+    cout << " Select the day of the week " << endl;
+    cout << endl;
+    cout << " 1.Monday " << endl;
+    cout << " 2.tuesday " << endl;
+    cout << " 3.wednesday " << endl;
+    cout << " 4.thursday " << endl;
+    cout << " 5.friday " << endl;
+    cout << " 6.saturday " << endl;
+    cout << " 7.sunday " << endl;
+
+}
+string getDay(dayofweek days)
+    {
+        switch(days)
+        {
+            case monday :
+                return "Monday";
+            break;
+              case tuesday :
+                return "Tuesday";
+            break;
+              case wednesday :
+                return "Wednesday";
+            break;
+              case thursday :
+                return "Thursday";
+            break;
+              case friday :
+                return "Friday";
+            break;
+              case saturday :
+                return "Saturday";
+            break;
+              case sunday :
+                return "Sunday";
+            break;
+
+        }
+    }
+
+
+int main()
+
+{
+    int indeks;
+    char ch;
+    do
+    {
+        do
+    {
+         menu();
+         cin >> indeks;
+         cout << getDay(dayofweek(indeks)) <<endl;
+         cout << "Do you want to continue the program? [Y/N]" << endl;
+         cin >> ch;
+         system("cls");
+    }
+    while( ch!= 'y' && ch!= 'Y' && ch!= 'n' && ch!= 'N');
+
+    }
+    while (ch=='y'||ch=='Y');
+
+
+
+
     return 0;
 }
